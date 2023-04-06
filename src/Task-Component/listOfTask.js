38dd94144a08970958c2c5644bb153df2f878918
Thierry Rakotomanana <ThierryRktm@gmail.com>
2023-04-06 22:53:09 +0300

    refactor structure of project

--- src/Task-Component/listOfTask.js
+++ src/Task-Component/listOfTask.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { List } from './list';
+import { List } from './task';
 import './Task.css'
 
 export function Task(){
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import {Task} from './taskList/Task';
+import {Task} from './Task-Component/listOfTask';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
