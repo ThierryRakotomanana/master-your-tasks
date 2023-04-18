1c3296f5f12192f0673dd894117ce1b7d0b3410a
Thierry Rakotomanana <ThierryRktm@gmail.com>
2023-04-18 14:37:48 +0300

    reaorganise priority of className

--- src/Task-Component/className.js
+++ src/Task-Component/className.js
@@ -1,7 +1,10 @@
 export const className = {
     div : 'p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4',
     texte : "text-slate-500",
-    input : "focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm",
+    header : "bg-white space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6",
+    divOfHeader : "flex items-center justify-between",
+    h2 : "font-semibold text-slate-900",
     a : "hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm",
     form : 'group relative',
+    input : "focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm",
 }
