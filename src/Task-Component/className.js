099cc31e150b6436452458dfcd3704ab35f951c5
Thierry Rakotomanana <ThierryRktm@gmail.com>
2023-04-18 14:20:13 +0300

    centralise all classname in one file

---
+++ src/Task-Component/className.js
@@ -0,0 +1,6 @@
+export const className = {
+    div : 'p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4',
+    texte : "text-slate-500",
+    input : "focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm",
+    a : "hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm",
+}
