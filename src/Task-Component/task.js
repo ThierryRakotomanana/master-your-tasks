5dc4ab9818202f2fadda23b1ec77c0ae96f114f4
ThierryRakotomanana <ThierryRktm@gmail.com>
2023-05-11 23:49:46 +0300

    make inline style

--- src/Task-Component/task.js
+++ src/Task-Component/task.js
@@ -10,10 +10,10 @@ export function List({value, deleteTask, editTask}){
                 newTitle && editTask(newTitle)
         }
         return (
-                <li className="">
-                     <span className={className.grid}>
-                        {show ? <span className={className.first}>{content} </span>: <input className={className.input + " " + className.first} value={content}  onChange={ (e) => setContent(e.target.value)}/>}
-                        <button type='button' className={className.button} onClick={deleteTask}>Delete</button> 
+                <li className="flex items-center justify-between">
+                     <span className="">
+                        {show ? <span className="text-slate-800">{content} </span>: <input className={className.input + " " + className.first} value={content}  onChange={ (e) => setContent(e.target.value)}/>}
+                        <button type='button' className="bg-blue-500 hover:bg-blue-200 text-slate-200 font-semibold hover:text-whhite py-1.5 px-2 rounded-md border-blue-500 hover:border-transparent rounded" onClick={deleteTask}>Delete</button> 
                         <Toggle onClick={() => changeEdit( show ? false : content)}>{show ? "Edit" : "Save your edit"}</Toggle>
                      </span> 
                 </li>
