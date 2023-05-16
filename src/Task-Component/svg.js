90269489b1874acb3be1183011bffeebdc7819e7
ThierryRaktomanana <ThierryRktm@gmail.com>
2023-05-16 13:27:12 +0300

    remove create-react-app

--- src/Task-Component/svg.js
+++
@@ -1,8 +0,0 @@
-export const svg =  {
-    a : <svg width="20" height="20" fill="currentColor" className="mr-2" ariaHidden="true">
-            <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
-        </svg>,
-    search : <svg width="20" height="20" fill="currentColor" className="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-500" aria-hidden="true">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
-            </svg>
-}
